57a29698b286423d4b2bcfb4b3810a717a11f334
XinNoil <xinwojiushiwokun@163.com>
2018-06-23 11:09:48 +0800

    modi sth

--- pre.m
+++ pre.m
@@ -2,4 +2,11 @@ work_path = pwd;
 addpath('./process');
 addpath('./setting files');
 addpath(genpath('./tools'));
-save('glo.mat','-append','work_path')
+if exist('./setting files/glo.mat','file')
+    save('./setting files/glo.mat','-append','work_path')
+    disp(['update work_path:' work_path]);
+else
+    save('./setting files/glo.mat','work_path')
+    disp('warning: glo.mat not found!');
+end
+    

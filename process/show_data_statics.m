963cef212de27b0621e4c1c002a6e8f274dcf983
XinNoil <xinwojiushiwokun@163.com>
2018-07-30 14:56:13 +0800

    add show_data_statics

---
+++ process/show_data_statics.m
@@ -0,0 +1,19 @@
+%% 输出参考点 测试点个数
+clear
+clc
+load glo.mat
+cd (work_path)
+disp(['data_version:' data_version]);
+
+areas=1:length(area_table);
+% 载入数据
+load (['data/fingerprints' data_version '.mat']);
+load (['data/testdatas' data_version '.mat']);
+fprintf('fp\t\ttd\t\tap\t\tarea\n');
+for area_i=areas
+    fp=fps{area_i};
+    td=tds{area_i}; 
+    if ~isempty(td)&&isfield(fp,'bssid_map')
+        fprintf('%s\t\t%s\t\t%s\t\t%s\n',n2s(fp.num),n2s(td.num),n2s(length(fp.bssid_map)),area_table{area_i});
+    end
+end
